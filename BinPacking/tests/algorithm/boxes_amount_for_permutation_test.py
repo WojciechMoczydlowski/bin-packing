eb283ca597d1bd4f7cfee703deab5c2b15fd5baf
WojciechMoczydlowski <wojciech.moczydlowski98@gmail.com>
2020-05-30 18:41:33 +0200

    Add first fit algorithm

--- BinPacking/tests/algorithm/boxes_amount_for_permutation_test.py
+++ BinPacking/tests/algorithm/boxes_amount_for_permutation_test.py
@@ -1,6 +1,7 @@
 import unittest
 
 from BinPacking.algorithms.brutal_bin_packing import boxes_amount_for_permutation
+from BinPacking.algorithms.first_fit_bin_packing import first_fit_bin_packing
 
 
 class MyTestCase(unittest.TestCase):
@@ -8,6 +9,11 @@ class MyTestCase(unittest.TestCase):
         permutation = [0.7, 0.9, 0.8, 0.2, 0.3, 0.7]
 
         self.assertEqual(boxes_amount_for_permutation(permutation), 4, "Permutation test")
+
+    def test_for_first_fit_test(self):
+        permutation = [0.7, 0.7, 0.7, 0.7, 0.3, 0.3, 0.3, 0.9, 0.3]
+
+        self.assertEqual(first_fit_bin_packing(permutation), 5, "Permutation test")
 
 
 if __name__ == '__main__':
